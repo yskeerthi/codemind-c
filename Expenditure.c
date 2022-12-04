87d177e2b04f1a1982b2566d9be8a652803d1ccc
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2022-12-04 22:08:35 +0530

    #include<stdio.h> int exp(int x,int y) {     if((x/30)/y)     {         return 1;     }     else     {         return 0;     } } int main() {     int x,y,a;     scanf("%d%d",&x,&y);     a=exp(x,y);     if(a==1)     {         printf("YES");     }     else     {         printf("NO");     } }

--- Expenditure.c
+++ Expenditure.c
@@ -1,14 +1,26 @@
 #include<stdio.h>
+int exp(int x,int y)
+{
+    if((x/30)/y)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 int main()
 {
-    int x,y;
+    int x,y,a;
     scanf("%d%d",&x,&y);
-    if((x/30)/y)
+    a=exp(x,y);
+    if(a==1)
     {
         printf("YES");
     }
     else
     {
-        printf("NO"); 
+        printf("NO");
     }
 }
