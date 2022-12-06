4c8810c58cb56f0d4701197936c8a403d33bbcba
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2022-12-06 18:50:22 +0530

    #include<stdio.h> int main() {     int c,a,b,x;     scanf("%d%d%d",&a,&b,&c);     for(b=b;b<=c;b++)     {         x=a*b;         printf("%d x %d = %d ",a,b,x);     } }

--- Multiplication_Table_3.c
+++ Multiplication_Table_3.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int c,i,a,b,x,count=0;
+    int c,a,b,x;
     scanf("%d%d%d",&a,&b,&c);
     for(b=b;b<=c;b++)
     {
         x=a*b;
-        count++;
         printf("%d x %d = %d
 ",a,b,x);
     }
