c5cc54c80e7f7224fb720fc1c12d7333f4edbc81
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2022-12-10 05:23:17 +0530

    #include<stdio.h> int main() {     int n=3,i,ca,c=1,s,t,b;    int ar[n]={t,s,b};     for(i=0;i<n;i++)     {        scanf("%d",&ar[i]);     }     for(i=0;i<n;i++)     {         c=c*ar[i];        ca=(2*c*512)/1024;     }     printf("%dKB",ca); }

--- Find_the_Capacity.c
+++ Find_the_Capacity.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int s,t,b,cap;
-    scanf("%d%d%d",&s,&t,&b);
-    cap=(2*s*t*b*512)/1024;
-    printf("%dKB",cap);
+    int n=3,i,ca,c=1,s,t,b;
+   int ar[n]={t,s,b};
+    for(i=0;i<n;i++)
+    {
+       scanf("%d",&ar[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=c*ar[i];
+       ca=(2*c*512)/1024;
+    }
+    printf("%dKB",ca);
 }
