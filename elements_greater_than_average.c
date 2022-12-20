5156a7b5110d1f7cfa6078050d542488208eca36
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2022-12-20 19:00:05 +0530

    #include<stdio.h> int main() {     int n,s=0,a[100],i,c=0;     float avrg;     scanf("%d",&n);     for(i=0;i<n;i++)     {          scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         s=s+a[i];     }     avrg=s/n;     for(i=0;i<n;i++)     {         if(a[i]>=avrg)       {         c++;       }     }     printf("%d",c);     return 0;
    }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0,a[100],i,c=0;
+    float avrg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+         scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+a[i];
+    }
+    avrg=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=avrg)
+      {
+        c++;
+      }
+    }
+    printf("%d",c);
+    return 0;
+    
+}
