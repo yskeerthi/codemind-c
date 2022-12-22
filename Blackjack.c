94b22e9deeda8154ff29354a5188bef813425d24
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2022-12-22 13:49:55 +0530

    #include<stdio.h> int main() {     int a,b,s,c;     scanf("%d%d",&a,&b);     s=a+b+c;     s=21;     c=21-a-b;     if(c>=1 && c<=10)     {        printf("%d",c);     }     else     {        printf("-1");     } }

---
+++ Blackjack.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,s,c;
+    scanf("%d%d",&a,&b);
+    s=a+b+c;
+    s=21;
+    c=21-a-b;
+    if(c>=1 && c<=10)
+    {
+       printf("%d",c); 
+    }
+    else 
+    {
+       printf("-1");
+    }
+}
