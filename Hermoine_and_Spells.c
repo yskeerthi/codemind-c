a5abc21bdaeb2d87ce19f88b63f9af0e478f7390
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2022-12-24 15:50:13 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a+b>b+c && a+b>a+c){         printf("%d",a+b);     }     else if(b+c>a+b && b+c>a+c)     {         printf("%d",b+c);     }     else     {         printf("%d",a+c);     } }

---
+++ Hermoine_and_Spells.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a+b>b+c && a+b>a+c){
+        printf("%d",a+b);
+    }
+    else if(b+c>a+b && b+c>a+c)
+    {
+        printf("%d",b+c);
+    }
+    else
+    {
+        printf("%d",a+c);
+    }
+}
