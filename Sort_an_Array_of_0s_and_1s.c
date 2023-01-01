3d66c1c6f0dacbb2771e374ab6b60b53594f8f1a
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2023-01-01 21:57:07 +0530

    #include<stdio.h> int main() {     int n,i,j,h,o,arr[1000],k[1000],s[1000],l[1000];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     j=0;     h=0;     o=0;     for(i=0;i<n;i++)     {         if(arr[i]==1)         {             k[j]=arr[i];             j+=1;         }         else         {             s[j]=arr[i];             h+=1;         }     }     for(i=0;i<h;i++)     {         l[o]=s[i];         o+=1;     }     for(i=0;i<j;i++)     {         l[o]=k[i];         o+=1;     }     for(i=0;i<n;i++)     {         printf("%d ",l[i]);     } }

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,h,o,arr[1000],k[1000],s[1000],l[1000];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    j=0;
+    h=0;
+    o=0;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==1)
+        {
+            k[j]=arr[i];
+            j+=1;
+        }
+        else
+        {
+            s[j]=arr[i]; 
+            h+=1;
+        }
+    }
+    for(i=0;i<h;i++)
+    {
+        l[o]=s[i];
+        o+=1;
+    }
+    for(i=0;i<j;i++)
+    {
+        l[o]=k[i];
+        o+=1;
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",l[i]);
+    }
+}
