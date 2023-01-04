c4b8462f3663e5ade6c957c05a5d47672c40106d
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2023-01-04 22:25:05 +0530

    #include <stdio.h> int main() {     int a,e,i,o,u;     scanf("%d%d%d%d%d",&a,&e,&i,&o,&u);     if(a>34 && i>34 && e>34 && o>34 && u>34 )     {         printf("PASSED");     }     else     {         printf("FAILED");     } }

---
+++ Passed_or_Failed.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,e,i,o,u;
+    scanf("%d%d%d%d%d",&a,&e,&i,&o,&u);
+    if(a>34 && i>34 && e>34 && o>34 && u>34 )
+    {
+        printf("PASSED");
+    }
+    else
+    {
+        printf("FAILED");
+    }
+}    
