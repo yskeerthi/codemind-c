f67214017492176d0b8e6ebf45b890cbd1d84c77
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2023-01-08 17:09:48 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=i;j<n;j++)         {             if(arr[j]==arr[i])             {                 c++;             }         }          if(c>n/2)             {                 printf("%d",arr[i]);             }     } }

---
+++ Majority_Element.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i;j<n;j++)
+        {
+            if(arr[j]==arr[i])
+            {
+                c++;
+            }
+        }
+         if(c>n/2)
+            {
+                printf("%d",arr[i]);
+            }
+    }
+}
