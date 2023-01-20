346ea033cc7e2c146cbe09f329d7f168e79df25d
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2023-01-20 20:45:43 +0530

    # include<stdio.h> #include<string.h> int main() {     char str[100];     int i,c=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]==' ')         {             c++;         }     }     printf("%d",c+1);     return 0; }

---
+++ Count_no._of_words.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    int i,c=0;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]==' ')
+        {
+            c++;
+        }
+    }
+    printf("%d",c+1);
+    return 0;
+}        
